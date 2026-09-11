9594b51eb87af30ee38fd44b82bbdad8e82f4891
sunlei <yemu2333@outlook.com>
2026-09-11 16:20:34 +0800

    fix: 补齐技术方案代码语言高亮

--- src/factories/argonCodeblockFactory.ts
+++ src/factories/argonCodeblockFactory.ts
@@ -1,8 +1,13 @@
 import hljs from 'highlight.js/lib/core'
 import bash from 'highlight.js/lib/languages/bash'
 import css from 'highlight.js/lib/languages/css'
+import dockerfile from 'highlight.js/lib/languages/dockerfile'
+import groovy from 'highlight.js/lib/languages/groovy'
+import ini from 'highlight.js/lib/languages/ini'
 import json from 'highlight.js/lib/languages/json'
+import nginx from 'highlight.js/lib/languages/nginx'
 import powershell from 'highlight.js/lib/languages/powershell'
+import python from 'highlight.js/lib/languages/python'
 import sql from 'highlight.js/lib/languages/sql'
 import typescript from 'highlight.js/lib/languages/typescript'
 import xml from 'highlight.js/lib/languages/xml'
@@ -10,8 +15,14 @@ import yaml from 'highlight.js/lib/languages/yaml'
 
 hljs.registerLanguage('bash', bash)
 hljs.registerLanguage('css', css)
+hljs.registerLanguage('dockerfile', dockerfile)
+hljs.registerLanguage('groovy', groovy)
+hljs.registerLanguage('ini', ini)
+hljs.registerAliases(['dotenv'], { languageName: 'ini' })
 hljs.registerLanguage('json', json)
+hljs.registerLanguage('nginx', nginx)
 hljs.registerLanguage('powershell', powershell)
+hljs.registerLanguage('python', python)
 hljs.registerLanguage('sql', sql)
 hljs.registerLanguage('typescript', typescript)
 hljs.registerAliases(['js', 'javascript', 'jsx', 'tsx'], { languageName: 'typescript' })
